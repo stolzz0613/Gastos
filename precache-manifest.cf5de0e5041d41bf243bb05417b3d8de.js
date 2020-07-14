03982f32f9749bc3f13b1d0e8ae5014251092ffb
stolzz0613 <andres0613@utp.edu.co>
2020-07-14 18:20:14 -0500

    Updates

---
+++ precache-manifest.cf5de0e5041d41bf243bb05417b3d8de.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "160bcf811cfbef09a6724f6d081fa02f",
+    "url": "/Gastos/index.html"
+  },
+  {
+    "revision": "e0915714f43fa363b95e",
+    "url": "/Gastos/static/css/main.9e44fd00.chunk.css"
+  },
+  {
+    "revision": "3fa2cf6ff3deba19950e",
+    "url": "/Gastos/static/js/2.b92f7d17.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Gastos/static/js/2.b92f7d17.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e0915714f43fa363b95e",
+    "url": "/Gastos/static/js/main.00ea2e5e.chunk.js"
+  },
+  {
+    "revision": "a54f4b39ae09af3b4b7d",
+    "url": "/Gastos/static/js/runtime-main.18ae09c8.js"
+  }
+]);
